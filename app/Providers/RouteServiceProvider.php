dda5957aee16743ab1f36fc8fa7e3aedab4356d3
youssefkhaled_74 <iyoussefbusinesstowork@gmail.com>
2026-07-09 11:51:33 +0300

    refactor: move the endpoints to the seperate file of the metwgo application

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -111,6 +111,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/metwgo.php'));
+
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
